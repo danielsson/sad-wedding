92d2f691f77c03450f0b4f23a3aa6ae5a23f606d
Mattias Danielsson <matdanie@kth.se>
2015-05-15 00:47:43 +0200

    Upgraded to express

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-var static = require('node-static');
+var express = require('express');
+var app = express();
 
-var fileServer = new static.Server('./public');
+app.set('view engine', 'jade');
+app.set('views', './views')
 
-require('http').createServer(function (request, response) {
-    request.addListener('end', function () {
-        fileServer.serve(request, response);
-    }).resume();
-}).listen(3000);
+app.use(express.static('public'))
+
+
+app.get('/', function (req, res) {
+  res.render('index');
+});
+
+var server = app.listen(process.env.PORT || 3000, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+
+});
