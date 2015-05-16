339342e0c8f5e9657894e0e23b0f84eaf3695b6e
Mattias Danielsson <matdanie@kth.se>
2015-05-16 16:48:36 +0200

    Added cache headers

--- index.js
+++ index.js
@@ -4,18 +4,30 @@ var app = express();
 app.set('view engine', 'jade');
 app.set('views', './views')
 
-app.use(express.static('public'))
+app.use(express.static('public', {
+    index: false,
+    maxAge: 31556900
+}))
+
+
+
+if (process.env.NODE_ENV == "production") {
+    app.use('/', function (req, res, next) {
+        res.setHeader("Cache-Control", "public, max-age=172800")
+        next()
+    })
+}
 
 
 app.get('/', function (req, res) {
-  res.render('index');
+    res.render('index');
 });
 
 var server = app.listen(process.env.PORT || 3000, function () {
 
-  var host = server.address().address;
-  var port = server.address().port;
+    var host = server.address().address;
+    var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+    console.log('Example app listening at http://%s:%s', host, port);
 
 });
